5346b2f68e3540d05d425f6ac844b7f580a8d922
Marcelo Soares <ceeloalves@hotmail.com>
2023-10-02 15:12:25 -0300

    corrigindo js

--- projeto/src/js/script.js
+++ projeto/src/js/script.js
@@ -1,18 +1,18 @@
 const botoes = document.querySelectorAll('.botao');
-         const personagens = document.querySelectorAll('.personagem')
+ const personagens = document.querySelectorAll('.personagem')
 
-         botoes.forEach((botao, index) =>{
-            botao.addEventListener("click", () => {
+botoes.forEach((botao, index) =>{
+    botao.addEventListener("click", () => {
                 
-                const personagemSelecionado = document.querySelector(".personagem.selecionado")
-                personagens[index].classList.add("selecionado")
-                personagemSelecionado.classList.remove("selecionado")
+        const personagemSelecionado = document.querySelector(".personagem.selecionado")
+        personagemSelecionado.classList.remove("selecionado")
+        personagens[index].classList.add("selecionado")
 
-                const botaoSelecionado = document.querySelector(".botao.selecionado")
-                botaoSelecionado.classList.remove("selecionado")
-                botao.classList.add("selecionado")
-            });
-         });
+        const botaoSelecionado = document.querySelector(".botao.selecionado")
+        botaoSelecionado.classList.remove("selecionado")
+        botao.classList.add("selecionado")
+    });
+});
 
 
 
